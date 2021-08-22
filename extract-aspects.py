4365408035c214a258dae0eda555f16c98e6488e
Daniel Torres <daniel.torrescandil@hotmail.com>
2021-08-22 21:15:07 +0200

    Update extract-aspects.py
    Review aspects added to dataset (just in execution, not stored in file)

--- extract-aspects.py
+++ extract-aspects.py
@@ -1,17 +1,26 @@
 import pandas as pd
 from spellchecker import SpellChecker
 
-df = pd.read_json("dataset/yelp_academic_dataset_review_sample.json", lines=True)
-df['aspects'] = None
+# Import datasets
+df_reviews = pd.read_json("dataset/yelp_academic_dataset_review_sample.json", lines=True)
+df_reviews['aspects'] = None
+df_aspects = pd.read_csv("dataset/aspects_restaurants.csv", header=None, names=['key', 'value'])
 
 spell = SpellChecker(language='en')
-punct_marks = ".,!?:;-()[]"
+punctuation_marks = ".,!?:;-()[]"
 
-for index, row in df.iterrows():
+# Convert 'aspects' dataframe into dictionary
+aspects_dict = {}
+for _, row in df_aspects.iterrows():
+    aspects_dict.setdefault(row['key'], [])
+    aspects_dict[row['key']].append(row['value'])
+
+# Filling 'aspects' attribute in 'reviews' dataset
+for index, row in df_reviews.iterrows():
     review = row['text']
 
     # Remove of punctuation marks in review
-    for char in punct_marks:
+    for char in punctuation_marks:
         review = review.replace(char, "")
 
     # Correct misspelled words
@@ -21,4 +30,12 @@ for index, row in df.iterrows():
         word = spell.correction(word)
         corrected_words.append(word)
 
-    print(corrected_words)
+    # Add the review's words to aspects if needed
+    aspects = []
+    for aspect in aspects_dict:
+        for a in aspects_dict[aspect]:
+            if a in corrected_words:
+                aspects.append(aspect)
+
+    aspects = list(set(aspects))
+    df_reviews.loc[index, 'aspects'] = str(aspects)
