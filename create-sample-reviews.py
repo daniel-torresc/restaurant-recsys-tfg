f3b7f3b489349d433ed8c218f75bd00265071d88
Daniel Torres <daniel.torrescandil@hotmail.com>
2021-08-23 18:56:08 +0200

    Aspects added to reviews dataset as new attribute

--- create-sample-reviews.py
+++ create-sample-reviews.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-df = pd.read_json("dataset/yelp_academic_dataset_review.json", lines=True)
-df = df[['review_id', 'user_id', 'business_id', 'stars', 'text']]
-sample = df.sample(10000)
-sample.to_json("dataset/yelp_academic_dataset_review_sample.json", orient='records', lines=True)
+if __name__ == "__main__":
+    df = pd.read_json("dataset/yelp_academic_dataset_review.json", lines=True)
+    df = df[['review_id', 'user_id', 'business_id', 'stars', 'text']]
+    sample = df.sample(10000)
+    sample.to_json("dataset/yelp_academic_dataset_review_sample.json", orient='records', lines=True)
--- extract-aspects.py
+++ extract-aspects.py
@@ -1,41 +1,39 @@
 import pandas as pd
-from spellchecker import SpellChecker
 
-# Import datasets
-df_reviews = pd.read_json("dataset/yelp_academic_dataset_review_sample.json", lines=True)
-df_reviews['aspects'] = None
-df_aspects = pd.read_csv("dataset/aspects_restaurants.csv", header=None, names=['key', 'value'])
+if __name__ == "__main__":
+    # Import datasets
+    df_reviews = pd.read_json("dataset/yelp_academic_dataset_review_sample.json", lines=True)
+    df_aspects = pd.read_csv("dataset/aspects_restaurants.csv", header=None, names=['key', 'value'])
 
-spell = SpellChecker(language='en')
-punctuation_marks = ".,!?:;-()[]"
+    punctuation_marks = ".,!?:;-()[]"
 
-# Convert 'aspects' dataframe into dictionary
-aspects_dict = {}
-for _, row in df_aspects.iterrows():
-    aspects_dict.setdefault(row['key'], [])
-    aspects_dict[row['key']].append(row['value'])
+    # Convert 'aspects' dataframe into dictionary
+    aspects_dict = {}
+    for _, row in df_aspects.iterrows():
+        aspects_dict.setdefault(row['key'], [])
+        aspects_dict[row['key']].append(row['value'])
 
-# Filling 'aspects' attribute in 'reviews' dataset
-for index, row in df_reviews.iterrows():
-    review = row['text']
+    # Filling 'aspects' attribute in 'reviews' dataset
+    for index, row in df_reviews.iterrows():
+        review = row['text']
 
-    # Remove of punctuation marks in review
-    for char in punctuation_marks:
-        review = review.replace(char, "")
+        # Remove of punctuation marks in review
+        for char in punctuation_marks:
+            review = review.replace(char, "")
 
-    # Correct misspelled words
-    review_words = review.lower().split()
-    corrected_words = []
-    for word in review_words:
-        word = spell.correction(word)
-        corrected_words.append(word)
+        # Check which aspects correspond to the review
+        review_words = review.lower().split()
+        aspects = []
+        for aspect in aspects_dict:
+            for a in aspects_dict[aspect]:
+                if a in review_words:
+                    aspects.append(aspect)
 
-    # Add the review's words to aspects if needed
-    aspects = []
-    for aspect in aspects_dict:
-        for a in aspects_dict[aspect]:
-            if a in corrected_words:
-                aspects.append(aspect)
+        aspects = list(set(aspects))
+        df_reviews.loc[index, 'aspects'] = str(aspects)
 
-    aspects = list(set(aspects))
-    df_reviews.loc[index, 'aspects'] = str(aspects)
+    # Drop reviews with no aspects from dataset
+    df_reviews.drop(df_reviews[df_reviews.aspects == "[]"].index, inplace=True)
+
+    # Dump 'aspects' into json file as new attribute
+    df_reviews.to_json("dataset/yelp_academic_dataset_review_sample.json", orient='records', lines=True)
