f2e5e29b08216533f70ef7896da807ac0610fc5b
Daniel Torres <daniel.torrescandil@hotmail.com>
2021-09-18 19:52:27 +0200

    Update nlp-processing.py
    Created 2 options for extracting lexicon and modifiers.

--- processing/nlp-processing.py
+++ processing/nlp-processing.py
@@ -4,59 +4,59 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 
 
 def get_associated_lexicon(term: str, aspect: str, adjectives: list, words: list, lexicon_dict: dict):
-    # TODO: check when the term is more than once in words
-    # num_occurrences = words.count(term)
-    # if num_occurrences == 1:
+    # Option 1: we check for the lexicon before the term or after the verb (two positions above the term)
+    # TODO: check when term or adjective are more than once in words
+    # term_index = words.index(term)
+    #
+    # for adjective in adjectives:
+    #     adjective_index = words.index(adjective)
+    #
+    #     # TODO: check after the verb, when the sentence is negated
+    #     if term_index - 1 == adjective_index or term_index + 2 == adjective_index:
+    #         if adjective in lexicon_dict[aspect]:
+    #             return adjective, lexicon_dict[aspect][adjective]
+    #
+    # return None, None
 
-    term_index = words.index(term)
-
+    # Option 2: no matter where the words are
     for adjective in adjectives:
-        adjective_index = words.index(adjective)
-        # TODO: check when the adjective is more than once in words
-
-        if term_index - 1 == adjective_index or term_index + 2 == adjective_index:  # TODO: check after the verb, when the sentence is negated
-            if adjective in lexicon_dict[aspect]:
-                return adjective, lexicon_dict[aspect][adjective]
+        if adjective in lexicon_dict[aspect]:
+            return adjective, lexicon_dict[aspect][adjective]
 
     return None, None
 
 
 def get_associated_modifier(lexicon: str, modifiers: list, words: list, modifiers_dict: dict):
-    lexicon_index = words.index(lexicon)
+    # Option 1: we check the modifier only on the previous position of the lexicon
+    # lexicon_index = words.index(lexicon)
+    # modifier_index = lexicon_index - 1 if lexicon_index > 0 else None
+    #
+    # if modifier_index:
+    #     modifier = words[modifier_index]
+    #
+    #     if modifier in modifiers and modifier in modifiers_dict:
+    #         return modifier, modifiers_dict[modifier]
+    #
+    # return None, None
 
+    # Option 2: no matter where the words are
     for modifier in modifiers:
-        modifier_index = words.index(modifier)
-
-        if lexicon_index - 1 == modifier_index:
-            if lexicon in modifiers_dict:
-                return modifier, modifiers_dict[lexicon]
+        if modifier in modifiers_dict:
+            return modifier, modifiers_dict[modifier]
 
     return None, None
 
 
 if __name__ == "__main__":
     # Import datasets
-    df_reviews = pd.read_json("dataset/yelp_academic_dataset_review_1k.json", lines=True)
-    df_aspects = pd.read_csv("dataset/aspects_restaurants.csv", header=None, names=['key', 'value'])
-    df_lexicon = pd.read_csv("dataset/lexicon_restaurants.csv", header=None, names=['aspect', 'lexicon', 'weight'])
-    df_modifiers = pd.read_csv("dataset/adjective-modifiers.csv", header=None, names=['modifier', 'weight'])
+    df_reviews = pd.read_json("../dataset/yelp_academic_dataset_review_1k.json", lines=True)
+    df_aspects = pd.read_csv("../dataset/aspects_restaurants.csv", header=None, names=['key', 'value'])
+    df_lexicon = pd.read_csv("../dataset/lexicon_restaurants.csv", header=None, names=['aspect', 'lexicon', 'weight'])
+    df_modifiers = pd.read_csv("../dataset/adjective-modifiers.csv", header=None, names=['modifier', 'weight'])
 
     # Create empty annotations dataframe
-    columns = ['user_id', 'review_id', 'rate', 'term', 'aspect', 'lexicon', 'modifier', 'feeling', 'lexicon_weight', 'modifier_weight']
+    columns = ['user_id', 'review_id', 'rate', 'term', 'aspect', 'lexicon', 'lexicon_weight', 'modifier', 'modifier_weight', 'feeling']
     annotations_df = pd.DataFrame(columns=columns)
-    # dtype = {
-    #     'user_id': str,
-    #     'review_id': str,
-    #     'rate': int,
-    #     'term': str,
-    #     'aspect': str,
-    #     'lexicon': str,
-    #     'modifier': str,
-    #     'feel': bool,
-    #     'lexicon_weight': int,
-    #     'modifier_weight': int
-    # }
-    # annotations_df.astype(dtype)
 
     # Convert 'aspects' dataframe into dictionary
     aspects_dict = {}
@@ -80,8 +80,8 @@ if __name__ == "__main__":
 
     # Iterate over each review
     for index, row in df_reviews.iterrows():
-        if (index + 1) % 10 == 0:
-            print(f"{index + 1} out of {len(df_reviews)}")
+        if (index + 1) % 50 == 0:
+            print(f"Processed {index + 1} out of {len(df_reviews)} reviews")
 
         user_id = row['user_id']
         review_id = row['review_id']
@@ -108,14 +108,14 @@ if __name__ == "__main__":
                         term = noun
 
                         lexicon, lexicon_weight = get_associated_lexicon(term, aspect, adjectives, words, lexicon_dict)
-                        modifier, modifier_weight = get_associated_modifier(lexicon, modifiers, words, modifiers_dict) if lexicon is not None else None, None
 
-                        new_record = [user_id, review_id, rate, term, aspect, lexicon, modifier, feeling,
-                                      lexicon_weight, modifier_weight]
+                        if lexicon is not None:
+                            modifier, modifier_weight = get_associated_modifier(lexicon, modifiers, words, modifiers_dict)
+                        else:
+                            modifier, modifier_weight = None, None
+
+                        new_record = [user_id, review_id, rate, term, aspect, lexicon, lexicon_weight, modifier, modifier_weight, feeling]
                         annotations_df.loc[len(annotations_df)] = new_record
 
-    # Dump annotations df into persisting file
-    # annotations_df.to_pickle("dataset/annotations_dataset.pkl")
-
     # Dump annotations into json file
-    annotations_df.to_json("dataset/annotations_dataset.json", orient='records', lines=True)
+    annotations_df.to_json("../dataset/annotations_dataset.json", orient='records', lines=True)
