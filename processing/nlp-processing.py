005afe8ca67a28f5f37f16498bf87631408a227f
Daniel Torres <daniel.torrescandil@hotmail.com>
2021-09-20 20:14:13 +0200

    Update nlp-processing.py

--- processing/nlp-processing.py
+++ processing/nlp-processing.py
@@ -3,22 +3,7 @@ import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 
-def get_associated_lexicon(term: str, aspect: str, adjectives: list, words: list, lexicon_dict: dict):
-    # Option 1: we check for the lexicon before the term or after the verb (two positions above the term)
-    # TODO: check when term or adjective are more than once in words
-    # term_index = words.index(term)
-    #
-    # for adjective in adjectives:
-    #     adjective_index = words.index(adjective)
-    #
-    #     # TODO: check after the verb, when the sentence is negated
-    #     if term_index - 1 == adjective_index or term_index + 2 == adjective_index:
-    #         if adjective in lexicon_dict[aspect]:
-    #             return adjective, lexicon_dict[aspect][adjective]
-    #
-    # return None, None
-
-    # Option 2: no matter where the words are
+def get_associated_lexicon(aspect: str, adjectives: list, lexicon_dict: dict):
     for adjective in adjectives:
         if adjective in lexicon_dict[aspect]:
             return adjective, lexicon_dict[aspect][adjective]
@@ -26,20 +11,7 @@ def get_associated_lexicon(term: str, aspect: str, adjectives: list, words: list
     return None, None
 
 
-def get_associated_modifier(lexicon: str, modifiers: list, words: list, modifiers_dict: dict):
-    # Option 1: we check the modifier only on the previous position of the lexicon
-    # lexicon_index = words.index(lexicon)
-    # modifier_index = lexicon_index - 1 if lexicon_index > 0 else None
-    #
-    # if modifier_index:
-    #     modifier = words[modifier_index]
-    #
-    #     if modifier in modifiers and modifier in modifiers_dict:
-    #         return modifier, modifiers_dict[modifier]
-    #
-    # return None, None
-
-    # Option 2: no matter where the words are
+def get_associated_modifier(modifiers: list, modifiers_dict: dict):
     for modifier in modifiers:
         if modifier in modifiers_dict:
             return modifier, modifiers_dict[modifier]
@@ -102,15 +74,13 @@ if __name__ == "__main__":
             adjectives = [word for word, tag in words_tagged if tag.startswith('J')]  # Finding adjectives
             modifiers = [word for word, tag in words_tagged if tag.startswith('RB')]  # Finding adverbs (modifiers)
 
-            for noun in nouns:  # Finding nouns
+            for term in nouns:
                 for aspect in aspects_dict:
-                    if noun in aspects_dict[aspect]:  # Checking if the noun correspond to one of our aspects
-                        term = noun
-
-                        lexicon, lexicon_weight = get_associated_lexicon(term, aspect, adjectives, words, lexicon_dict)
+                    if term in aspects_dict[aspect]:  # Checking if the noun correspond to one of the aspects
+                        lexicon, lexicon_weight = get_associated_lexicon(aspect, adjectives, lexicon_dict)
 
                         if lexicon is not None:
-                            modifier, modifier_weight = get_associated_modifier(lexicon, modifiers, words, modifiers_dict)
+                            modifier, modifier_weight = get_associated_modifier(modifiers, modifiers_dict)
                         else:
                             modifier, modifier_weight = None, None
 
