05b3b0736c009f02d423d887505fec26cb7d68bc
Daniel Torres <daniel.torrescandil@hotmail.com>
2021-10-24 16:11:48 +0200

    Update ratings.py
    Changed some attributes names and main now in another separated class

--- recommender/ratings.py
+++ recommender/ratings.py
@@ -1,20 +1,18 @@
-import pandas as pd
 
 
 class Ratings:
 
     def __init__(self, df):
-        self.aspects = sorted(df['aspect'].unique())  # list of all aspects in the reviews
-        self.restaurants = sorted(df['restaurant_id'].unique())  # list of all restaurants
-        self.users = sorted(df['user_id'].unique())  # list of all users
+        self.all_restaurants = sorted(df['restaurant_id'].unique())  # list of all restaurants
+        self.all_users = sorted(df['user_id'].unique())  # list of all users
 
-        self.user_aspects = {}  # weighted aspects for each user
-        self.restaurant_aspects = {}  # weighted aspects for each restaurant
+        self.user_ratings = {}  # weighted aspects for each user
+        self.restaurant_ratings = {}  # weighted aspects for each restaurant
 
         # Calculate the weight of each aspect for every user
-        for user in self.users:
-            aux_df = df.loc[df['user_id'] == user]
-            self.user_aspects[user] = {}
+        for user in self.all_users:
+            aux_df = df.loc[df['user_id'] == user]  # Create subset with only the rows where user == user_id
+            self.user_ratings[user] = {}
 
             for _, row in aux_df.iterrows():
                 aspect = row['aspect']
@@ -22,27 +20,39 @@ class Ratings:
                 # TODO: hacer una buena ponderacion entre el rate y el feeling
                 weighted_score = 0.6 * row['rate'] + 0.4 * row['feeling'] * 5
 
-                self.user_aspects[user].setdefault(aspect, weighted_score)
-                self.user_aspects[user][aspect] = 0.5*self.user_aspects[user][aspect] + 0.5*weighted_score
+                self.user_ratings[user].setdefault(aspect, weighted_score)
+                self.user_ratings[user][aspect] = 0.5 * self.user_ratings[user][aspect] + 0.5 * weighted_score
 
         # Calculate the weight of each aspect for every restaurant
-        for restaurant in self.restaurants:
+        for restaurant in self.all_restaurants:
             aux_df = df.loc[df['restaurant_id'] == restaurant]
 
-            self.restaurant_aspects[restaurant] = {}
+            self.restaurant_ratings[restaurant] = {}
             for _, row in aux_df.iterrows():
                 aspect = row['aspect']
 
                 # TODO: hacer una buena ponderacion entre el rate y el feeling
                 weighted_score = 0.6 * row['rate'] + 0.4 * row['feeling'] * 5
 
-                self.restaurant_aspects[restaurant].setdefault(aspect, weighted_score)
-                self.restaurant_aspects[restaurant][aspect] = 0.5 * self.restaurant_aspects[restaurant][aspect] + 0.5 * weighted_score
+                self.restaurant_ratings[restaurant].setdefault(aspect, weighted_score)
+                self.restaurant_ratings[restaurant][aspect] = 0.5 * self.restaurant_ratings[restaurant][aspect] + 0.5 * weighted_score
 
+    def rating(self, item, aspect):
+        if item in self.users():
+            return self.user_ratings[item][aspect]
+        elif item in self.restaurants():
+            return self.restaurant_ratings[item][aspect]
+        else:
+            return None
 
-if __name__ == "__main__":
-    df_annotations = pd.read_json("../dataset/annotations_dataset.json", lines=True)
+    def user_aspects(self, user):
+        return self.user_ratings[user] if user in self.user_ratings else None
 
-    ratings = Ratings(df_annotations)
+    def restaurant_aspects(self, restaurant):
+        return self.restaurant_ratings[restaurant] if restaurant in self.restaurant_ratings else None
 
-    print("DONE")
+    def users(self):
+        return self.all_users
+
+    def restaurants(self):
+        return self.all_restaurants
