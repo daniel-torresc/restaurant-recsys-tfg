80e7a96cfd69fc5d300a94ace34562ea8b2e5ec5
Daniel Torres <daniel.torrescandil@hotmail.com>
2022-01-07 17:04:37 +0100

    Update metrics.py

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -10,25 +10,19 @@ class Metric(ABC):
         self.threshold = threshold
 
     def __repr__(self):
-        return f"{type(self).__name__}" + f"@{self.cutoff}" if self.cutoff else ""
+        return f"{type(self).__name__}" + (f"@{self.cutoff}" if self.cutoff else "") + f" threshold={self.threshold}"
 
     @abstractmethod
     def compute(self, recommendation):
         """ Core computing function of the evaluation metrics """
 
-    def get_relevant_recommendations(self, item, recommendations):
+    def get_relevant_recommendations(self, user, recommendations):
         relevant_recommendations = 0  # A recommendation is relevant when the rating set by the user >= threshold
 
         for index, rec in enumerate(recommendations, start=1):
-            try:
-                if item in self.test.users():
-                    if self.test.user_rating(item, rec[0]) >= self.threshold:
-                        relevant_recommendations += 1
-                elif item in self.test.restaurants():
-                    if self.test.restaurant_rating(item, rec[0]) >= self.threshold:
-                        relevant_recommendations += 1
-            except ValueError:
-                continue
+            rate = self.test.user_rating(user, rec[0])
+            if rate and rate >= self.threshold:
+                relevant_recommendations += 1
 
             if index == self.cutoff:
                 break
@@ -43,52 +37,42 @@ class Precision(Metric):
 
     def compute(self, recommendation):
         precision_list = []
-        for item in recommendation:
-            relevant_recommendations = self.get_relevant_recommendations(item, recommendation[item])
-            precision = relevant_recommendations / min(self.cutoff, len(recommendation[item]))
+        for user in recommendation:
+            relevant_recommendations = self.get_relevant_recommendations(user, recommendation[user])
+            precision = relevant_recommendations / min(self.cutoff, len(recommendation[user]))
 
             precision_list.append(precision)
 
-        return statistics.mean(precision_list)  # average of all precision metrics
+        return statistics.mean(precision_list), statistics.stdev(precision_list)
 
 
 class Recall(Metric):
     def __init__(self, test, cutoff, threshold):
-        super().__init__(test, cutoff. cutoff, threshold)
+        super().__init__(test, cutoff, threshold)
 
         self.relevant_user = {}
         for user in self.test.users():
             self.relevant_user[user] = 0
 
             for rating in self.test.ratings(user):
-                if rating >= self.threshold:
+                if self.test.ratings(user)[rating] >= self.threshold:
                     self.relevant_user[user] += 1
-
-        self.relevant_restaurant = {}
-        for restaurant in self.test.restaurants():
-            self.relevant_restaurant[restaurant] = 0
-
-            for rating in self.test.ratings(restaurant):
-                if rating >= self.threshold:
-                    self.relevant_restaurant[restaurant] += 1
 
     def compute(self, recommendation):
         recall_list = []
-        for item in recommendation:
+        for user in recommendation:
             try:
-                relevant_recommendations = self.get_relevant_recommendations(item, recommendation[item])
-                recall = relevant_recommendations / self.get_num_relevants(item)
+                relevant_recommendations = self.get_relevant_recommendations(user, recommendation[user])
+                recall = relevant_recommendations / self.get_num_relevants(user)
             except ZeroDivisionError:
                 recall = 0
 
             recall_list.append(recall)
 
-        return statistics.mean(recall_list)  # average of all recall metrics
+        return statistics.mean(recall_list), statistics.stdev(recall_list)
 
-    def get_num_relevants(self, item):
-        if item in self.test.users():
-            return self.relevant_user[item]
-        elif item in self.test.restaurants():
-            return self.relevant_restaurant[item]
-        else:
+    def get_num_relevants(self, obj):
+        try:
+            return self.relevant_user[obj]
+        except KeyError:
             return 0
